14e50b387d3f00ea0de01c16d975489208a4ff05
sayyaddawood <dawood@plutus21.com>
2022-12-23 08:48:04 +0500

    Signed-off-by: sayyaddawood <dawood@plutus21.com>

--- application/config/database.php
+++ application/config/database.php
@@ -3,10 +3,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $active_group = 'default';
 $query_builder = TRUE;
+/*
+$db['default']['hostname'] = 'localhost';
+/// $db['default']['port'] = 25060;
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'pti_members';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['pconnect'] = FALSE;
+*/
 
-$db['default'] = array(
+$db['default']['hostname'] = 'db-mysql-sgp1-12709-do-user-12232967-0.b.db.ondigitalocean.com:25060';
+//$db['default']['port'] = 25060;
+$db['default']['username'] = 'doadmin';
+$db['default']['password'] = 'AVNS_VD2DSeDufXiFeiUsqnL';
+$db['default']['database'] = 'hyd_dmr';
+$db['default']['dbdriver'] = 'mysqli';
+$db['default']['pconnect'] = FALSE;
+
+
+/* $db['default'] = array(
 	'dsn'	=> '',
-	// 'port'=>25060,
 	'hostname' => 'db-mysql-sgp1-12709-do-user-12232967-0.b.db.ondigitalocean.com:25060',
 	'username' => 'doadmin',
 	'password' => 'AVNS_VD2DSeDufXiFeiUsqnL',
@@ -14,7 +31,7 @@ $db['default'] = array(
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
-	'db_debug' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
@@ -25,4 +42,4 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
-);
+); */
